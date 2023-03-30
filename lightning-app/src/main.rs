4b06dcaaa7fb1dbdffa05b3b38bdd3dd15509912
Helyos96 <67038113+Helyos96@users.noreply.github.com>
2023-03-30 23:30:56 +0200

    Remove the automatic cmd window

--- lightning-app/src/main.rs
+++ lightning-app/src/main.rs
@@ -1,5 +1,6 @@
 // todo: remove this once stable-ish
 #![allow(dead_code)]
+#![windows_subsystem = "windows"]
 
 //! A basic self-contained example to get you from zero-to-demo-window as fast
 //! as possible.
