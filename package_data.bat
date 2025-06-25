6b6fd59f372f556e1f0a016bedbf312bf3372a3a
Helyos96 <67038113+Helyos96@users.noreply.github.com>
2025-06-25 21:44:25 +0200

    3.26 data

--- package_data.bat
+++ package_data.bat
@@ -1,5 +1,5 @@
-python lightning-model\scripts\cleanup_gems.py ..\RePoE-lvlvllvlvllvlvl\RePoE\data\gems.json > lightning-model\data\gems.json
-python lightning-model\scripts\cleanup_items.py ..\RePoE-lvlvllvlvllvlvl\RePoE\data\base_items.json > lightning-model\data\base_items.json
+python lightning-model\scripts\cleanup_gems.py ..\RePoE-lvlvllvlvllvlvl\data\gems.json > lightning-model\data\gems.json
+python lightning-model\scripts\cleanup_items.py ..\RePoE-lvlvllvlvllvlvl\data\base_items.json > lightning-model\data\base_items.json
 xcopy ..\skilltree-export\data.json data.json
 python lightning-model\scripts\fix_ascendancy_positions.py
 python lightning-model\scripts\cleanup_tree.py data.json > lightning-model\data\tree.json
